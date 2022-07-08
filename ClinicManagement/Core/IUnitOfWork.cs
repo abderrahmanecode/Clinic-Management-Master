a4f1d08936b1a7ab3de93d1863afc9c62ad92f5d
Abderrahmane ABOUZIAD <abderrahman.abouziad@gmail.com>
2022-07-08 11:50:17 +0100

    Add project files.

---
+++ ClinicManagement/Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ClinicManagement.Core.Repositories;
+
+namespace ClinicManagement.Core
+{
+    public interface IUnitOfWork
+    {
+        IPatientRepository Patients { get; }
+        IAppointmentRepository Appointments { get; }
+        IAttendanceRepository Attandences { get; }
+        ICityRepository Cities { get; }
+        IDoctorRepository Doctors { get; }
+        ISpecializationRepository Specializations { get; }
+        IApplicationUserRepository Users { get; }
+
+        void Complete();
+    }
+}
